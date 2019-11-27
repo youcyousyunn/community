c9cb237d47ed0ed92289a553764668e88126070e
youcyousyunn <731781984@qq.com>
2019-11-27 17:46:25 +0800

    用户相关

---
+++ src/main/java/com/ycs/community/sysbo/domain/po/UserPo.java
@@ -0,0 +1,55 @@
+package com.ycs.community.sysbo.domain.po;
+
+import com.ycs.community.basebo.domain.po.BasePo;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class UserPo extends BasePo implements UserDetails {
+    private Long id;
+    private Long accountId;
+    private String name;
+    private String password;
+    private String token;
+    private String avatarUrl;
+    private boolean enabled;
+    private Long lastPasswordResetTime;
+    private List<RolePo> roles;
+    private DeptPo dept;
+    private JobPo job;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
