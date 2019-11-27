4f9b8a499a9a09c574bcd51b145d8dd4130a089c
youcyousyunn <731781984@qq.com>
2019-11-27 17:45:51 +0800

    用户相关

---
+++ src/main/java/com/ycs/community/sysbo/domain/dto/QryUserPageRequestDto.java
@@ -0,0 +1,19 @@
+package com.ycs.community.sysbo.domain.dto;
+
+import com.ycs.community.basebo.domain.dto.BaseRequestDto;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+public class QryUserPageRequestDto extends BaseRequestDto {
+    private String name;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date startTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endTime;
+    private Boolean enabled;
+    private Integer currentPage;
+    private Integer pageSize;
+}
