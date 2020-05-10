4732b747e264d329c1879c4401e8ac529c22019d
youcyousyunn <731781984@qq.com>
2020-05-10 14:14:41 +0800

    加入Elasticsearch测试代码

--- src/test/java/com/ycs/community/ApplicationTest.java
+++ src/test/java/com/ycs/community/ApplicationTest.java
@@ -1,6 +1,7 @@
 package com.ycs.community;
 
 import com.ycs.community.sysbo.activemq.producer.QueueProducer;
+import com.ycs.community.sysbo.domain.po.UserPo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -14,11 +15,26 @@ import java.util.UUID;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
-
 	protected final Logger logger = LoggerFactory.getLogger(ApplicationTest.class);
 
 	@Autowired
 	private QueueProducer queueProducer;
+//	@Autowired
+//	private JestClient jestClient;
+
+	@Test
+	public void testSearch() {
+		String filter = "{\n" +
+				"	\"query\" : {\n" +
+				"		\"match\" : {\n" +
+				"			\"usrNm\" : \"张三丰\n" +
+				"		|\n" +
+				"	}\n" +
+				"}";
+//		Search search = new Search.builder(filter).addIndex("eladmin")
+//				.addType("user").build();
+//		jestClient.execute(search);
+	}
 
 	@Test
 	public void testSend() {
