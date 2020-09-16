1c82f603706794900fb042b161fc575d0026c7ec
youcyousyunn <731781984@qq.com>
2020-09-16 09:31:00 +0800

    更新启动类

--- src/main/java/com/ycs/community/StartupApplication.java
+++ src/main/java/com/ycs/community/StartupApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.EnableAsync;
 
